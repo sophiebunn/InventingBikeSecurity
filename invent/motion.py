5d6ac0e20380f239ff8f9f6967c1318694bfba4d
Sophie <112041730+sophiebunn@users.noreply.github.com>
2025-03-19 11:36:09 -0700

    working stream: need to fix picam to cv2 convert

---
+++ invent/motion.py
@@ -0,0 +1,40 @@
+from picamera2 import Picamera2
+
+import cv2
+import numpy as np
+import time
+
+def motion_detection(frame, ctrl):  
+    mtn = False
+
+    #making image black & white, NECESSARY for thresholding and contouring
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #increase the Gaussian blur to decrease the amount of arbitrary noise between frames
+    gray = cv2.GaussianBlur(gray, (33, 33), 0)
+    
+    #comparing the control frame to the current frame
+    frameDelta = cv2.absdiff(ctrl, gray)
+    
+    #make the threshold according to the difference between frames
+    #this will return an image made black and white where white is the movement
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)    
+    
+    #find the contours (shapes) that are changing between frames
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        
+    for contour in cnts:
+        area = cv2.contourArea(contour)
+
+        #yes, this is a less than sign
+        #making area less than EXCLUDES small contours
+        if area < 900:
+            mtn = True
+            continue
+
+        #drawing a green rectangle around the moving objects in our original frame
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    return mtn
